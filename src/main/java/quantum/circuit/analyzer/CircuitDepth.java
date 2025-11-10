7f7506a544f7fb58d08e018d4ce2030eb7c1fdba
dongheeHan <ark182818@gmail.com>
2025-11-10 21:42:54 +0900

    feat: CircuitDepth 구현
    - 회로 깊이 계산 (Step 개수)
    - static 메서드로 간단한 인터페이스 제공
    - 깊이는 회로의 총 Step 개수와 동일

---
+++ src/main/java/quantum/circuit/analyzer/CircuitDepth.java
@@ -0,0 +1,10 @@
+package quantum.circuit.analyzer;
+
+import quantum.circuit.domain.circuit.QuantumCircuit;
+
+public class CircuitDepth {
+
+    public static int calculate(QuantumCircuit circuit) {
+        return circuit.getStepCount();
+    }
+}
